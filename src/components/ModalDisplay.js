26a106009e377669b8a923f147d8bdb78ffaac77
Chan Kar Seng <chankarseng906@gmail.com>
2021-10-29 16:39:10 +0800

    Add conditional rendering of the modal display Add free streaming service tab

--- src/components/ModalDisplay.js
+++ src/components/ModalDisplay.js
@@ -28,7 +28,12 @@ import {
 } from '@chakra-ui/react';
 import { getFlagEmoji } from '../utils/getFlagEmoji';
 const ModalDisplay = ({ title, isOpen, onClose, keys, watch_providers }) => {
-  const modalSize = useBreakpointValue({ base: 'sm', lg: '3xl' });
+  const modalSize = useBreakpointValue({
+    base: 'sm',
+    sm: 'md',
+    md: 'lg',
+    lg: '3xl',
+  });
   if (keys.length === 0 || keys === undefined || keys === []) {
     return (
       <Modal isOpen={isOpen} onClose={onClose} size={modalSize}>
@@ -47,6 +52,9 @@ const ModalDisplay = ({ title, isOpen, onClose, keys, watch_providers }) => {
       </Modal>
     );
   } else {
+    const allWatchProviders = keys.map((country) => {
+      return watch_providers[country];
+    });
     return (
       <Modal isOpen={isOpen} onClose={onClose} size={modalSize}>
         <ModalOverlay />
@@ -56,14 +64,33 @@ const ModalDisplay = ({ title, isOpen, onClose, keys, watch_providers }) => {
           </ModalHeader>
           <ModalCloseButton />
           <ModalBody>
-            <Tabs isLazy isFitted variant="enclosed" defaultIndex={0}>
+            <Tabs isLazy isFitted defaultIndex={0}>
               <TabList>
                 <Tab>Streaming</Tab>
-                <Tab>Streaming w/ Ads</Tab>
+                <Tab
+                  isDisabled={
+                    allWatchProviders.filter((w) => w.hasOwnProperty('ads'))
+                      .length === 0
+                      ? true
+                      : false
+                  }
+                >
+                  Streaming w/ Ads
+                </Tab>
+                <Tab
+                  isDisabled={
+                    allWatchProviders.filter((w) => w.hasOwnProperty('free'))
+                      .length === 0
+                      ? true
+                      : false
+                  }
+                >
+                  Free
+                </Tab>
               </TabList>
               <TabPanels>
                 <TabPanel>
-                  <Table size="lg" variant="simple" colorScheme="blackAlpha">
+                  <Table size="sm" variant="simple" colorScheme="blackAlpha">
                     <Thead>
                       <Tr>
                         <Th>Country</Th>
@@ -121,7 +148,7 @@ const ModalDisplay = ({ title, isOpen, onClose, keys, watch_providers }) => {
                   </Table>
                 </TabPanel>
                 <TabPanel>
-                  <Table size="lg" variant="simple" colorScheme="purple">
+                  <Table size="sm" variant="simple" colorScheme="purple">
                     <Thead>
                       <Tr>
                         <Th>Country</Th>
@@ -151,6 +178,62 @@ const ModalDisplay = ({ title, isOpen, onClose, keys, watch_providers }) => {
                               <Td>
                                 <Wrap>
                                   {countryStreamingService.ads.map(
+                                    (service) => {
+                                      return (
+                                        <WrapItem key={service.provider_name}>
+                                          <Image
+                                            title={service.provider_name}
+                                            loading="lazy"
+                                            alt={service.provider_name}
+                                            m={2}
+                                            boxSize="60px"
+                                            src={`https://image.tmdb.org/t/p/w500/${service.logo_path}`}
+                                          />
+                                        </WrapItem>
+                                      );
+                                    }
+                                  )}
+                                </Wrap>
+                              </Td>
+                            </Tr>
+                          );
+                        }
+                        return null;
+                      })}
+                    </Tbody>
+                  </Table>
+                </TabPanel>
+                <TabPanel>
+                  <Table size="sm" variant="simple" colorScheme="cyan">
+                    <Thead>
+                      <Tr>
+                        <Th>Country</Th>
+                        <Th>Streaming Service Available</Th>
+                      </Tr>
+                    </Thead>
+                    <Tbody>
+                      {keys.map((country) => {
+                        const countryStreamingService =
+                          watch_providers[country];
+                        if (countryStreamingService.hasOwnProperty('free')) {
+                          return (
+                            <Tr key={country}>
+                              <Td>
+                                <Flex direction={{ base: 'column', md: 'row' }}>
+                                  <Text fontSize={30}>
+                                    {getFlagEmoji(country)}
+                                  </Text>
+                                  <Text
+                                    ml={{ base: 0, md: 5 }}
+                                    mt={{ base: 5, md: 0 }}
+                                  >
+                                    ({country})
+                                  </Text>
+                                </Flex>
+                              </Td>
+                              <Td>
+                                <Wrap>
+                                  {countryStreamingService.free.map(
                                     (service) => {
                                       return (
                                         <WrapItem key={service.provider_name}>
