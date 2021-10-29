e6ce722afac94a3717765d1e8db8c9b4077d4581
Chan Kar Seng <chankarseng906@gmail.com>
2021-10-29 23:31:16 +0800

    Fix mobile view to better accomodate smaller screen sizes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,18 +7,23 @@ import {
   IconButton,
   Text,
   useColorModeValue,
+  useMediaQuery,
 } from '@chakra-ui/react';
 import { Link as RouterLink } from 'react-router-dom';
 import { SiGithub, SiThemoviedatabase } from 'react-icons/si';
 
 const Footer = (props) => {
+  const [isMobileSize] = useMediaQuery(
+    '(min-height: 750px), (min-width: 30em)'
+  );
+  console.log(isMobileSize);
   return (
     <Box
       bg={useColorModeValue('gray.50', 'gray.900')}
       color={useColorModeValue('gray.700', 'gray.200')}
       // position={'fixed'}
       position={
-        props.itemCount !== 0 && props.component === 'home'
+        (props.itemCount !== 0 && props.component === 'home') || !isMobileSize
           ? 'relative'
           : 'fixed'
       }
