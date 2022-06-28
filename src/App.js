4fd44f0f3c72755dd25d7a018cfdcaecdd4e2543
Chan Kar Seng <chankarseng96@gmail.com>
2022-06-28 16:30:08 +0800

    [Fix] - Fixed build error

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
 import React, { useState } from 'react';
 import Home from './components/Home';
-import { Switch, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import About from './components/About';
 import Faq from './components/Faq';
 import { Flex } from '@chakra-ui/react';
 const App = () => {
-  const [itemCount, setItemCount] = useState(0);
-  const updateItemCount = (count) => {
-    setItemCount(count);
-  };
-  return (
-    <Switch>
-      <Route path="/faq">
-        <Flex minH="100vh" flexDirection="column">
-          <Faq />
-          <Footer component="faq" />
-        </Flex>
-      </Route>
-      <Route path="/about">
-        <Flex minH="100vh" flexDirection="column">
-          <About />
-          <Footer component="about" />
-        </Flex>
-      </Route>
-      <Route path="/">
-        <Flex minH="100vh" flexDirection="column">
-          <Home setItemCount={updateItemCount} />
-          <Footer itemCount={itemCount} component="home" />
-        </Flex>
-      </Route>
-      <Footer />
-    </Switch>
-  );
+    const [itemCount, setItemCount] = useState(0);
+    const updateItemCount = (count) => {
+        setItemCount(count);
+    };
+    return (
+        <Routes>
+            <Route
+                path="/faq"
+                element={
+                    <Flex minH="100vh" flexDirection="column">
+                        <Faq />
+                        <Footer component="faq" />
+                    </Flex>
+                }></Route>
+            <Route
+                path="/about"
+                element={
+                    <Flex minH="100vh" flexDirection="column">
+                        <About />
+                        <Footer component="about" />
+                    </Flex>
+                }></Route>
+            <Route
+                path="/"
+                element={
+                    <Flex minH="100vh" flexDirection="column">
+                        <Home setItemCount={updateItemCount} />
+                        <Footer itemCount={itemCount} component="home" />
+                    </Flex>
+                }></Route>
+            {/* <Footer /> */}
+        </Routes>
+    );
 };
 
 export default App;
